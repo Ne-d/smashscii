5e3ca2865740693cb2a37f3beeb540794f28efb5
Ned <67144261+Ne-d@users.noreply.github.com>
2024-09-16 17:07:38 +0200

    Add the entire game trust me bro

--- Smashscii/Smashscii.cpp
+++ Smashscii/Smashscii.cpp
@@ -2,9 +2,33 @@
 //
 
 #include <iostream>
+#include <Windows.h>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    CONSOLE_SCREEN_BUFFER_INFO csbiInfo;
+    
+    HANDLE hStdin = GetStdHandle(STD_INPUT_HANDLE);
+    HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+    if (hStdin == INVALID_HANDLE_VALUE ||
+        hStdout == INVALID_HANDLE_VALUE)
+    {
+        MessageBox(NULL, TEXT("GetStdHandle"), TEXT("Console Error"),
+            MB_OK);
+        return 1;
+    }
+
+
+    if (!GetConsoleScreenBufferInfo(hStdout, &csbiInfo))
+    {
+        MessageBox(NULL, TEXT("GetConsoleScreenBufferInfo"),
+            TEXT("Console Error"), MB_OK);
+        return 1;
+    }
+
+    std::cout << csbiInfo.dwSize.X << std::endl;
 }
 
+//  [ O ]
+//  [-|-]
+//  [/ \]
