006e1b98bf93946f7b436d556a2d7bd20f5e5aff
Ned <67144261+Ne-d@users.noreply.github.com>
2024-09-23 13:47:57 +0200

    Complete cleanup.

--- Source/Engine/Vector2D.h
+++ Source/Engine/Vector2D.h
@@ -5,13 +5,16 @@
 class Vector2D
 {
 public:
+	// Constructors
 	Vector2D() = default;
 	Vector2D(float x, float y);
-	Vector2D(const Vector2D& other);
-	
+	Vector2D(const Vector2D& other) = default;
+
+	// Vector elements (exceptionally public)
 	float x = 0.f;
 	float y = 0.f;
-	
+
+	// Utility
 	COORD RoundToCoord() const;
 
 	// Operators
